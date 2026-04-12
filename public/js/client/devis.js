55fa2a8d46520fbd5f3b3084de1a7b6311807987
RGamz <rgamzat@live.com>
2026-04-12 14:43:30 +0200

    fix scrollview behaviour

--- public/js/client/devis.js
+++ public/js/client/devis.js
@@ -403,7 +403,7 @@ function renderEstimate() {
   document.getElementById('question-area').classList.add('hidden');
   document.getElementById('contact-form-area').classList.add('hidden');
   area.classList.remove('hidden');
-  area.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 
   // Build summary items
   const summaryItems = [
